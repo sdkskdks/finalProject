3facfe273ad7556fde5c187a197b725c6b8220d5
Someone You loved <202082@astanait.edu.kz>
2022-03-04 01:36:37 +0600

    Heroku set up

--- imageNetProj/settings.py
+++ imageNetProj/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q_6-wyh5m^%br*#(14bnm_43hi9&y^+j@#zikqa6(-$jyw*f+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn imageNetProj.wsgi --log-file -
