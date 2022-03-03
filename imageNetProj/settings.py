428ee4a60d5425543ab5667e74d73a7bf5a2584e
Someone You loved <a.beksultan.17@mail.ru>
2022-03-04 02:01:16 +0600

    +

--- imageNetProj/settings.py
+++ imageNetProj/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -63,10 +64,10 @@ WSGI_APPLICATION = 'imageNetProj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
+        'NAME': 'd1t2donk1el9m1',
+        'USER': 'llponbsnkqzzos',
+        'PASSWORD': '3e2710cf69ff7a2a4677bc850d8dfcb137be957d69ca797ba86b578b2b1188d7',
+        'HOST': 'ec2-176-34-105-15.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -104,6 +105,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
